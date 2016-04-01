3c038b2d570d42ce86afaa64f3e519dacdd9c818
Jason Burns <admiralmudkip@gmail.com>
2016-03-31 22:01:13 -0700

    a

--- app/src/main/java/ca/bcit/cst/jason/mapspindrop/MapsActivity.java
+++ app/src/main/java/ca/bcit/cst/jason/mapspindrop/MapsActivity.java
@@ -1,5 +1,6 @@
 package ca.bcit.cst.jason.mapspindrop;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -38,9 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        Location l = mMap.getMyLocation();
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(l.getLatitude(), l.getLongitude())).title("It's Me!"));
     }
 }
